1d238699b7db7a66b38f923e89c0b018765e66c3
Heather Larsson <jessicalrssn@gmail.com>
2022-02-09 17:15:01 -0700

    javascript

--- chamber/js/banner.js
+++ chamber/js/banner.js
@@ -2,13 +2,3 @@
  //"🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m."
  //Make sure your design matches your schema for the site.
 
- //input=computer will get date
- let now = new Date();
- let dayOfWeek= now.getDay();
-
- //processing:check if the day is 1 or 2?
- if (dayOfWeek ==1 || dayOfWeek == 2 ){
-  let banner = document.getElementById('banner');
- }
-
- //output
