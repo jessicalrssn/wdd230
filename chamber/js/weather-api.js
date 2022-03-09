44a561d106c83b485276b2bb97d4ebacdcf1ce06
Heather Larsson <jessicalrssn@gmail.com>
2022-03-08 17:02:28 -0700

    weather

---
+++ chamber/js/weather-api.js
@@ -0,0 +1,14 @@
+const apiURL= "https://api.openweathermap.org/data/2.5/weather?q=Logan&units=imperial&appid=76fda5fbdea32e8ea0ce839e840b62d5"
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+     document.querySelector('#t').textContent = jsObject.main.temp.toFixed(1);
+
+const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+const desc = jsObject.weather[0].description;
+document.querySelector('#weathericon').setAttribute('src', iconsrc);
+document.querySelector('#weathericon').setAttribute('alt', desc);
+document.querySelector('figcaption').textContent = desc;
+  });
