da3803186cad4b43586601bfc60f07ac7b2bd92b
Heather Larsson <jessicalrssn@gmail.com>
2022-03-08 17:25:44 -0700

    more weather

--- chamber/js/weather-api.js
+++ chamber/js/weather-api.js
@@ -8,7 +8,10 @@ fetch(apiURL)
 
 const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
 const desc = jsObject.weather[0].description;
+const windspeed = jsObject.wind.speed;
+
 document.querySelector('#weathericon').setAttribute('src', iconsrc);
 document.querySelector('#weathericon').setAttribute('alt', desc);
 document.querySelector('figcaption').textContent = desc;
+document.querySelector('#s').textContent= windspeed;
   });
--- lesson10/weatherapi.js
+++ lesson10/weatherapi.js
@@ -8,10 +8,13 @@ fetch(apiURL)
 
 const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
 const desc = jsObject.weather[0].description;
+const w= jsObject.wind;
 document.querySelector('#icon-src').textContent = iconsrc;
 document.querySelector('#weathericon').setAttribute('src', iconsrc);
 document.querySelector('#weathericon').setAttribute('alt', desc);
 document.querySelector('figcaption').textContent = desc;
+document.querySelector('#wind').setAttribute('src', w);
+
   });
 
 
