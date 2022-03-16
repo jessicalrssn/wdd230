0861571aaf486a990eb29c3bc0146c8857c01762
Heather Larsson <jessicalrssn@gmail.com>
2022-03-15 20:51:38 -0600

    spotlights

--- chamber/js/spotlight.js
+++ chamber/js/spotlight.js
@@ -1,8 +1,6 @@
 const requestURL = 'https://jessicalrssn.github.io/wdd230/chamber/data.json';
 
 const spot1= document.querySelector('.spot1');
-const spot2= document.querySelector('.spot2');
-const spot3= document.querySelector('.spot3');
 
 fetch(requestURL)
     .then(function (response) {
@@ -14,36 +12,44 @@ fetch(requestURL)
         // console.table(jsonObject); //temporary checking for valid response and data parsing
          const golddirectory = directory.filter(x => (x.level === "gold"));
          golddirectory.forEach(displayDirectory);
+
+        
+          spot1(golddirectory, directory)
+          
     });
 
  function displayDirectory(directory) { //create elements to add to the document
   let spot1 = document.createElement('section');
 
+
   let h2 = document.createElement('h2');
   h2.textContent = `${directory.name}`;
-  card.appendChild(h2);
+  spot1.appendChild(h2);
+ 
 
 
   let p1 = document.createElement('p');
   p1.textContent = ` ${directory.address}`;
-  card.appendChild(p1);
+  spot1.appendChild(p1);
+  
 
   
-  let p2 = document.createElement('p');
-  p2.textContent = `${directory.phone}`;
-  card.appendChild(p2);
+ 
+  
 
     let p3 = document.createElement('p');
   p3.textContent = `${directory.website}`;
-  card.appendChild(p3);
+  spot1.appendChild(p3);
+  
 
   //add images
   let image = document.createElement('img');
   image.src = directory.imageurl;
   image.setAttribute('alt', directory.order);
-  card.appendChild(image);
+  spot1.appendChild(image);
 
 
   document.querySelector('div.spot1').appendChild(spot1);
+ 
 
 }
