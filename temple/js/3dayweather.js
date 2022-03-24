989d822c7c31dade590611fc6807ac85458931b9
Heather Larsson <jessicalrssn@gmail.com>
2022-03-24 15:09:39 -0600

    home responsive

---
+++ temple/js/3dayweather.js
@@ -0,0 +1,53 @@
+const apiURL3= "https://api.openweathermap.org/data/2.5/forecast?lat=41&lon=12&units=imperial&appid=76fda5fbdea32e8ea0ce839e840b62d5"
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+      console.log(jsObject);
+     document.querySelector('#t3').textContent = jsObject.main.temp.toFixed(0);
+
+const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+const desc = jsObject.weather[0].description;
+const humid = jsObject.main.humidity;
+
+document.querySelector('#weathericon3').setAttribute('src', iconsrc);
+document.querySelector('#weathericon3').setAttribute('alt', desc);
+document.querySelector('figcaption').textContent = desc;
+document.querySelector('#h3').textContent= humid;
+
+  });
+
+
+  fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+      console.log(jsObject);
+     document.querySelector('#t2').textContent = jsObject.main.temp.toFixed(0);
+
+const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+const desc = jsObject.weather[0].description;
+const humid = jsObject.main.humidity;
+
+document.querySelector('#weathericon2').setAttribute('src', iconsrc);
+document.querySelector('#weathericon2').setAttribute('alt', desc);
+document.querySelector('figcaption').textContent = desc;
+document.querySelector('#h2').textContent= humid;
+
+  });
+
+  fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+      console.log(jsObject);
+     document.querySelector('#t1').textContent = jsObject.main.temp.toFixed(0);
+
+const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+const desc = jsObject.weather[0].description;
+const humid = jsObject.main.humidity;
+
+document.querySelector('#weathericon1').setAttribute('src', iconsrc);
+document.querySelector('#weathericon1').setAttribute('alt', desc);
+document.querySelector('figcaption').textContent = desc;
+document.querySelector('#h1').textContent= humid;
+
+  });
--- weathertest.js
+++ weathertest.js
@@ -9,29 +9,14 @@ fetch(apiURL)
 const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
 const desc = jsObject.weather[0].description;
 const windspeed = jsObject.wind.speed.toFixed(0);
+const h= jsObject.humidity;
 
 document.querySelector('#weathericon').setAttribute('src', iconsrc);
 document.querySelector('#weathericon').setAttribute('alt', desc);
 document.querySelector('figcaption').textContent = desc;
-document.querySelector('#s').textContent= windspeed;
+document.querySelector('#h').textContent= windspeed;
 
-let t= parseFloat(document.querySelector("#t").textContent);
-let s= parseFloat(document.querySelector("#s").textContent);
-let windchill="";
 
-if (t <= 50 && s > 3) {
-    windchill = windChill(t, s);
-    windchill =`${windchill}&#176; F`;
-}else{
-    windchill="N/A";
-}
-
-document.querySelector("#w").innerHTML = windchill;
-
-function windChill(temp, speed){
-     let a = Math.pow(speed, .16);
-               let f = 35.74 + 0.6215 * temp - 35.75 * a + 0.4275 * temp * a;
-               return f.toFixed(0);}
   
   });
 
