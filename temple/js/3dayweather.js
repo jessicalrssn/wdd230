b8fda4547d29ef0ac6a26fea7f8571e19255447f
Heather Larsson <jessicalrssn@gmail.com>
2022-04-01 19:56:30 -0600

    weather

--- temple/js/3dayweather.js
+++ temple/js/3dayweather.js
@@ -1,10 +1,10 @@
-const apiURL3= "https://api.openweathermap.org/data/2.5/forecast?lat=41&lon=12&units=imperial&appid=76fda5fbdea32e8ea0ce839e840b62d5"
+const apiURL3= "http://api.openweathermap.org/data/2.5/onecall?lat=41&lon=12&dt=1586468027&units=imperial&appid=76fda5fbdea32e8ea0ce839e840b62d5"
 
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
       console.log(jsObject);
-     document.querySelector('#t3').textContent = jsObject.main.temp.toFixed(0);
+     //document.querySelector('#t3').textContent = jsObject.temp.toFixed(0);
 
 const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
 const desc = jsObject.weather[0].description;
@@ -51,3 +51,5 @@ document.querySelector('figcaption').textContent = desc;
 document.querySelector('#h1').textContent= humid;
 
   });
+
+"https://api.openweathermap.org/data/2.5/forecast?lat=41&lon=12&units=imperial&appid=76fda5fbdea32e8ea0ce839e840b62d5"
